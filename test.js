64113f03941fc614913fa91e4b3d0ca44e5b06ae
Simon Roberts <simon@dancingcloudservices.com>
2017-02-27 12:00:34 -0500

    array iteration example

--- test.js
+++ test.js
@@ -1,43 +1,12 @@
-//"use strict";
-console.log("Hello, World!");
+"use strict";
 
-function doStuff() {
-// console.log("number is undefine? " + (number === undefined));
-// console.log(number);
-	number = 99;
-	console.log("finished");
+let myArray = ["hello", "goodbye"];
+myArray.__proto__.newStuff = "hello, how's that for a surprise";
+for (let k in myArray) {
+	console.log("key is " + k);
+	console.log(" - value is " + myArray[k]);
 }
 
-function showObject(obj) {
-	for (let k in obj) {
-		console.log("found key " + k);
-		console.log("  - value is " + obj[k]);
-	}
+for (let i = 0; i < myArray.length; i++) {
+	console.log("indexing give: " + myArray[i]);
 }
-
-doStuff();
-console.log("number is " + number);
-console.log("finished everything");
-
-let anObject = {
-		[Symbol.iterator]: function() {},// blah blah
-		name: "fred"
-};
-console.log("anObject.name is " + anObject.name);
-let na = "na";
-let me = "me";
-console.log("anObject['name'] is " + anObject["name"]);
-console.log("anObject[na + me] is " + anObject[na + me]);
-
-console.log("anObject.newThing is " + anObject.newThing);
-
-showObject(anObject);
-
-anObject.newThing = "a new string";
-console.log("anObject.newThing is " + anObject.newThing);
-
-showObject(anObject);
-
-delete anObject.newThing;
-console.log('deleted newthing');
-showObject(anObject);
